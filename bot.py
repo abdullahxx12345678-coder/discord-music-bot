b652c968190acc25de7820c4e56afb3e70a35e95
abdullahxx12345678-coder <abdullahxx12345678@gmail.com>
2026-04-21 20:41:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ intents.voice_states = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ================= LAVALINK SETTINGS =================
-LAVALINK_URL = "https://lavalink-server-tlb3.onrender.com"
+LAVALINK_URL = "https://lavalink-server-tlb3.onrender.com/"
 LAVALINK_PASSWORD = "12345678"
 
 # ================= LAVALINK CONNECT =================
